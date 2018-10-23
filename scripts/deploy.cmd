86f3bb68ea16de019bcfc973e3a7c0ebc748139e
eram <ethanram@gmail.com>
2018-10-24 00:25:24 +0200

    fix gcp deploy

--- scripts/deploy.cmd
+++ scripts/deploy.cmd
@@ -1,30 +1,32 @@
 @echo off
 
-npx version-bump-prompt --patch --commit --tag --push
+call npx version-bump-prompt --patch --commit --tag --push
 if errorlevel 1 goto err
 
 node -p "require('./package.json').version" > %temp%\tmp-ver
 set /P REACT_APP_VERSION=<%temp%\tmp-ver
 del %temp%\tmp-ver
+echo REACT_APP_VERSION=%REACT_APP_VERSION%
 
 set REACT_APP_ENDPOINT=http://tensorflow-stack-ts.appspot.com/stack/api/graphql
 set REACT_APP_HEALTHCHECK=http://tensorflow-stack-ts.appspot.com/stack/api/_healthcheck
 
-npm run clean
+call npm run clean
 if errorlevel 1 goto err
 
 cd client
-npm run build
+call npm run build
 if errorlevel 1 goto err
 
 cd ..
-npm run build
+call npm run build
 if errorlevel 1 goto err
 
-gcloud app deploy gcp.prod.yaml
+call gcloud app deploy gcp.prod.yaml
 if errorlevel 1 goto err
 
 echo done ok
+call gcloud app browse
 exit 0
 
 :err
