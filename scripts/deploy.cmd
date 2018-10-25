2b122a55ac3b2405aeb0d507ccd9d2932e129d98
eram <ethanram@gmail.com>
2018-10-26 02:24:41 +0300

    gcp deployment configs fixes

--- scripts/deploy.cmd
+++ scripts/deploy.cmd
@@ -8,9 +8,10 @@ set /P REACT_APP_VERSION=<%temp%\tmp-ver
 del %temp%\tmp-ver
 echo REACT_APP_VERSION=%REACT_APP_VERSION%
 
+set DEPLOY_TO=https://tensorflow-stack-ts.appspot.com
 set PUBLIC_URL=/app
-set REACT_APP_ENDPOINT=stack/api/graphql
-set REACT_APP_HEALTHCHECK=/stack/api/_healthcheck
+set REACT_APP_ENDPOINT=%DEPLOY_TO%/stack/api/graphql
+set REACT_APP_HEALTHCHECK=%DEPLOY_TO%/stack/api/_healthcheck
 
 call npm run clean
 if errorlevel 1 goto err
@@ -26,8 +27,8 @@ if errorlevel 1 goto err
 call gcloud app deploy gcp.prod.yaml
 if errorlevel 1 goto err
 
-echo done ok
-start %PUBLIC_URL%
+echo deploy ok
+start %DEPLOY_TO%
 exit 0
 
 :err
